7a3db45cce1c9ccb82ded08d25ec3b088975efec
pepito <pepitoperez@gmail.com>
2024-09-16 21:02:26 -0500

    Preparando pruebas unitarias

---
+++ src/test/java/com/mycompany/parcial1/PrincipalTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.parcial1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PrincipalTest {
+    
+    public PrincipalTest() {
+    }
+
+    /**
+     * Test of main method, of class Principal.
+     */
+    @Test
+    public void testMain() {
+    }
+    
+}
